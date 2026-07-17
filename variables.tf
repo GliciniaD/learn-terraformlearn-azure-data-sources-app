7e5cc5ab9e959ac48e216968a4737bc1dbef9f25
GD <glicinia95@gmail.com>
2026-07-17 15:39:54 +0100

    variables

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "instances_per_subnet" {
+  description = "Number of VM instances to deploy per private subnet"
+  type        = number
+  default     = 1
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to the SSH public key file for VM admin login"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
